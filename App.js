e7b32aa4805fc5c2cc3512d94f53f1254225ca22
brightosei059 <bosei059@st.ug.edu.gh>
2024-07-06 09:59:04 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is BRIGHT OSEI</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+  fontSize: 24,
+  color: '#000',
+ }
+});
