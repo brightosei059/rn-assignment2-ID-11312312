473b9d3c019d3ae6dfeaaab18e920e0df4691f3e
brightosei059 <bosei059@st.ug.edu.gh>
2024-07-06 10:47:48 +0000

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is BRIGHT OSEI</Text>
+      <Text>My name is BRIGHT OSEI</Text> style={styles.boldText}>
       <StatusBar style="auto" />
     </View>
   );
